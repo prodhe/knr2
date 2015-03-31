184f5850f8b5b06f5b03db2381fba67e8173fe3f
Petter Rodhelind <petter.rodhelind@gmail.com>
2015-03-31 19:01:12 +0200

    ex1-5

--- chapter01/fahr-celsius.c
+++ chapter01/fahr-celsius.c
@@ -8,6 +8,12 @@ int main() {
     fahr2cels();
     printf("\nCelsius to Fahrenheit:\n");
     cels2fahr();
+
+    printf("Fahrenheit to Celsius (using for and in reverse):\n");
+    int fahr;
+    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+        printf("%3d %6.1f\n", fahr, ((fahr - 32.0) / 1.8));
+
 }
 
 void fahr2cels() {
