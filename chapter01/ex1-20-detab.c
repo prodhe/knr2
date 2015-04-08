33f1a37d5e7d35f3c5a5466de06f7c1197440fdc
Petter Rodhelind <petter.rodhelind@gmail.com>
2015-04-08 20:37:56 +0200

    solved and using command line arg for tabstop value

--- chapter01/ex1-20-detab.c
+++ chapter01/ex1-20-detab.c
@@ -7,7 +7,50 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#define MAXLINE 1000
 
-int main(void) {
+int detab_line(char s[], int lim, int tabstop);
+
+/* main: detab - turn every tab into proper number of blanks */
+int main(int argc, char *argv[]) {
+    char line[MAXLINE];
+    int len, rows, tabstop;
+
+    if (argc > 0) {
+        tabstop = atoi(argv[1]);
+    } else {
+        tabstop = 8;
+    }
+
+    rows = 0; 
+    while ((len = detab_line(line, MAXLINE, tabstop)) > 0) {
+        ++rows;
+        printf("%s", line);
+    }
+
     return 0;
+}
+
+/* getline: read a line into s, return length */
+int detab_line(char s[], int lim, int tabstop) {
+    int c, i;
+
+    for (i=0; i < lim-tabstop-1 && (c=getchar()) != EOF && c != '\n'; ++i) {
+        if (c == '\t') {
+            s[i] = ' ';
+            while ((i+1) % tabstop > 0) {
+                ++i;
+                s[i] = ' ';
+            }
+        } else {
+            s[i] = c;
+        }
+    }
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
 }
